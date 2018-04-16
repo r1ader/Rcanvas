2795887901975f8df83c169e14d557d2eaff7b59
r1ader <r1ader@qq.com>
2018-04-16 20:26:02 +0800

    default

--- Rcanvas.js
+++ Rcanvas.js
@@ -341,6 +341,46 @@ function update_Objects(update_type) {
 }
 //Math Part (end)
 
+//bezier (begin)
+function PointOnCubicBezier( cp, t )
+{
+    var   ax, bx, cx;
+    var   ay, by, cy;
+    var   tSquared, tCubed;
+    var   result = new Point() ;
+
+    /*計算多項式係數*/
+
+    cx = 3.0 * (cp[1].x - cp[0].x);
+    bx = 3.0 * (cp[2].x - cp[1].x) - cx;
+    ax = cp[3].x - cp[0].x - cx - bx;
+
+    cy = 3.0 * (cp[1].y - cp[0].y);
+    by = 3.0 * (cp[2].y - cp[1].y) - cy;
+    ay = cp[3].y - cp[0].y - cy - by;
+
+    /*計算位於參數值t的曲線點*/
+
+    tSquared = t * t;
+    tCubed = tSquared * t;
+
+    result.x = (ax * tCubed) + (bx * tSquared) + (cx * t) + cp[0].x;
+    result.y = (ay * tCubed) + (by * tSquared) + (cy * t) + cp[0].y;
+
+    return result;
+}
+
+function ComputeBezier(cp, numberOfPoints, curve )
+{
+    var   dt;
+    var   i;
+
+    dt = 1.0 / ( numberOfPoints - 1 );
+
+    for( i = 0; i < numberOfPoints; i++)
+        curve[i] = PointOnCubicBezier( cp, i*dt );
+}
+//bezier (end)
 
 
 document.onkeydown = function (event) {
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
+
+* 2018/4/16  添加了贝塞尔曲线模块 
+
 以下是基于此库写的一些小动画
 
 * [克苏鲁怪物](https://r1ader.github.io/Rcanvas/demo_eye.html)
